377b00d911913af4ea80e78b7148750c2bf4563d
Hugo Rodrigues Mota <hugomota.iblue@ciclic.com.br>
2024-01-17 18:00:51 -0300

    kaka

---
+++ grud-test/src/components/AppFormResume.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-for="item in Object.keys(modelValue)">
+    <p>{{ item }}</p>
+    <span></span>
+  </div>
+
+  {{ modelValue }}
+</template>
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  modelValue: {
+    type: Object,
+    default: {},
+  },
+});
+</script>
